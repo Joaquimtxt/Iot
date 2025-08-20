02c1745e44fa4fecab983d025d39625d90a99f9b
Joaquim <jocaq124@gmail.com>
2025-08-20 11:39:35 -0300

    Alterado os tópicos para o meu nome

--- UmidadeETemperatura/src/main.jsx
+++ UmidadeETemperatura/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
 import App from './App.jsx'
+import { ThemeProvider } from './contexts/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+          <App />
+          </ThemeProvider>
   </StrictMode>,
 )
